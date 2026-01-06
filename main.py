86962ba5a7361d81a72d622a5166de2e603f6c78
WeChatScanHelper <167905791+WeChatScanHelper@users.noreply.github.com>
2026-01-06 20:43:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from telethon import TelegramClient, events, errors, functions, types
 from telethon.sessions import StringSession
 
 # ================= CONFIG =================
-API_ID = int(os.getenv("API_ID", 30973213))
-API_HASH = os.getenv("API_HASH", "4a14f8a15197ed8f7c7cb24704beea51")
+API_ID = int(os.getenv("API_ID", 33117894))
+API_HASH = os.getenv("API_HASH", "6e0f6dc3472fcfcab0bba0c001e36f3c")
 SESSION_STRING = os.getenv("SESSION_STRING")
 
 GROUP_TARGET = -1003621946413
-MY_NAME = "Yuri"
+MY_NAME = "Hikari"
 BOT_USERNAME = "FkerKeyRPSBot"
 
 # ================= STATE =================
